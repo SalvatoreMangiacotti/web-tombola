44494de1f01c8d91091bdeb82be04cb13590b3bc
Salvatore <workstudioms@gmail.com>
2024-12-24 03:48:50 +0100

    web-tombola core features: random number extraction, display drawn numbers on page, and buttons state management , complete

--- script.js
+++ script.js
@@ -1,1 +1,86 @@
-console.log("Hello World");
+// 1. Estrazione dei numeri
+
+// Al click sul tasto Estrai verrà estratto un numero a caso dall’array dei numeri disponibili.
+// Questo numero andrà  mostrato e la casella corrispondente verrà colorata in modo da segnalarne l’estrazione.
+// Non dimenticate di rimuovere il numero estratto dalla lista dei numeri estraibili per evitare doppioni!!
+
+// 1.1 Gestione della lista numeri
+
+const listaNumeri = document.querySelectorAll('#lista_numeri li');
+const arrayListaNumeri = [];
+const arrayNumeriEstratti = [];
+
+for (i = 0; i < listaNumeri.length; i++) {
+
+    const indiceListaNumeri = Number(listaNumeri[i].textContent);
+    arrayListaNumeri.push(indiceListaNumeri);
+
+}
+
+console.log(arrayListaNumeri);
+
+
+
+// 1.2 Estrazione e gestione di numero random dall'array visibile in pagina
+
+const numeroEstratto = document.getElementById('numero_estratto');
+
+function estrazioneNumeroRandom() {
+
+    const max = arrayListaNumeri.length;
+    const indiceNumeroRandom = Math.floor(Math.random() * max);
+
+    const numeroRandomEstratto = arrayListaNumeri.splice(indiceNumeroRandom, 1)[0];
+    arrayNumeriEstratti.push(numeroRandomEstratto);
+
+    numeroEstratto.textContent = numeroRandomEstratto;
+
+    listaNumeri[indiceNumeroRandom].style.backgroundColor = '#45c5f8';
+
+}
+
+
+
+// 1.3 Funzionalità bottone estrai
+
+const bottoneEstrai = document.getElementById('bottone_estrai');
+
+bottoneEstrai.addEventListener('click', () => {
+
+    estrazioneNumeroRandom();
+
+    if (arrayNumeriEstratti.length >= 15) {
+
+        bottoneEstrai.disabled = true;
+        bottoneTermina.disabled = false;
+
+    }
+
+    console.log(arrayNumeriEstratti);
+    console.log(arrayListaNumeri);
+
+})
+
+
+
+// 2. Fine Gioco
+
+// Il gioco finisce automaticamente quando vengono estratti tutti i numeri o se si clicca sul tasto “Termina Gioco”.
+// Il tasto “Termina gioco” non può essere cliccato prima che vengano estratti 15 numeri *(ogni cartella della tombola è composta da 15 numeri pertanto nessuno può vincere prima che ne vengano estratti almeno 15)*
+// Quando il gioco finisce, mostriamo una schermata che annuncia che un giocatore ha fatto Tombola!
+
+// 2.1 Funzionalità bottone termina partita
+
+const bottoneTermina = document.getElementById('bottone_termina');
+
+bottoneTermina.disabled = true;
+
+bottoneTermina.addEventListener('click', () => {
+
+    if (arrayNumeriEstratti.length >= 15) {
+
+        window.location.reload();
+
+    }
+
+})
